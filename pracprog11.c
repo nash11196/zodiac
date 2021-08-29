4ef92165ca7f42588148af18ce4d449a6261bd67
Nazmul Hasan Shaon <87071175+nash11196@users.noreply.github.com>
2021-08-29 09:48:11 +0600

    Add files via upload

---
+++ pracprog11.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    int value;
+
+    printf("Please enter your number: ");
+    scanf("%d", &num);
+
+    value = num * 1;
+    printf("%d X 1 = %d\n", num, value);
+
+
+    value = num * 2;
+    printf("%d X 2 = %d\n", num, value);
+
+    value = num * 3;
+    printf("%d X 3 = %d\n", num, value);
+
+    value = num * 4;
+    printf("%d X 4 = %d\n", num, value);
+
+    value = num * 5;
+    printf("%d X 5 = %d\n", num, value);
+
+    value = num * 6;
+    printf("%d X 6 = %d\n", num, value);
+
+    value = num * 7;
+    printf("%d X 7 = %d\n", num, value);
+
+    value = num * 8;
+    printf("%d X 8 = %d\n", num, value);
+
+    value = num * 9;
+    printf("%d X 9 = %d\n", num, value);
+
+    value = num * 10;
+    printf("%d X 10 = %d\n", num, value);
+
+
+    return 0;
+
+
+}
