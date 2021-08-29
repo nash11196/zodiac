315d1d882a278ad905f02390b6030877df56b7e5
Nazmul Hasan Shaon <87071175+nash11196@users.noreply.github.com>
2021-08-29 09:50:50 +0600

    Add files via upload

---
+++ prognash2.c
@@ -0,0 +1,130 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int name;
+    char ch;
+    printf("\a");
+    printf("Please enter your name: ");
+    scanf("%c", &name);
+    printf("\a");
+
+    if(name=='A'){
+        printf("You are a good person!\n");
+    }
+    else if(name=='a'){
+        printf("You are a brilliant person!\n");
+    }
+    else if(name=='B'){
+        printf("You are a lucky person!\n");
+    }
+    else if(name=='b'){
+        printf("You will change the world!\n");
+    }
+    else if(name=='C'){
+        printf("You are a talented person!\n");
+    }
+    else if(name=='c'){
+      printf("Your talent is god gifted!\n");
+    }
+    else if(name=='D'){
+        printf("Your personality is too good!\n");
+    }
+    else if(name=='d'){
+        printf("You are a smart person!\n");
+    }
+    else if(name=='E'){
+        printf("Try your best, you will shine in life!\n");
+    }
+    else if(name=='e'){
+        printf("You are a good person but your luck is too bad!\n");
+    }
+    else if(name=='F'){
+        printf("You will shine in life but before that you have to struggle for it!\n");
+    }
+    else if(name=='f'){
+        printf("Everyone will ignore but you have to concentrate to your aim!\n");
+    }
+    else if(name=='H'){
+        printf("When you will be all grown up, you will be a big fatty person!\n");
+    }
+    else if(name=='h'){
+        printf("Stay strong,surely you will success in life!\n");
+    }
+    else if(name=='I'){
+        printf("You are a really a nice person!\n");
+    }
+    else if(name=='i'){
+        printf("Work hard,you will get your success in your career!\n");
+    }
+    else if(name=='J'){
+        printf("Yes only you can do it, just try it!\n");
+    }
+    else if(name=='j'){
+        printf("This is your lucky day!\n");
+    }
+    else if(name=='K'){
+        printf("You are a such a bad person!\n");
+    }
+    else if(name=='k'){
+        printf("You are a misbehavior!\n");
+    }
+    else if(name=='L'){
+        printf("Never get so high!\n");
+    }
+    else if(name=='l'){
+        printf("Don't try to be smart, because when you try to be smart you look like a dumb!\n");
+    }
+    else if(name=='M'){
+        printf("Don't be a hero,be brave!\n");
+    }
+    else if(name=='m'){
+        printf("Crying don't suit you, so try to be strong!\n");
+    }
+    else if(name=='N'){
+        printf("You are a good person, you will be successful in life. Just be patient!\n");
+    }
+    else if(name=='n'){
+        printf("You are the most handsome person in the world!\n");
+    }
+    else if(name=='O'){
+        printf("Win or learn how to win!\n");
+    }
+    else if(name=='o'){
+        printf("Don't show your dumb face to me!\n");
+    }
+    else if(name=='P'){
+        printf("You are not a tough guy!So don't try to be tough!\n");
+    }
+    else if(name=='p'){
+        printf("Your face proved that you are a bad person!\n");
+    }
+    else if(name=='R'){
+        printf("Don't show your pretty face because everybody knows you are a bad person from inside!\n");
+    }
+    else if(name=='r'){
+        printf("Try to respect everyone like your parents!\n");
+    }
+    else if(name=='S'){
+        printf("Never lag behind!\n");
+    }
+    else if(name=='s'){
+        printf("Concentrate to your career not to any other thing!\n");
+    }
+    else if(name=='T'){
+        printf("Don't think that you got everything you want, in future you have to suffer for this!\n");
+    }
+    else if(name=='t'){
+        printf("Learn to respect people, people will respect you too!\n");
+    }
+    else if(name=='W'){
+        printf("You'll be a great leader in future!\n");
+    }
+    else if(name=='Z'){
+        printf("Be attentive to your study!\n");
+    }
+    else if(name=='z'){
+        printf("Never lose to anyone\n");
+    }
+    return 0;
+}
